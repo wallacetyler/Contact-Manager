886ee83892542db35dc9b23cb45795effdf8614b
sSasquatch <37203953+sSasquatch@users.noreply.github.com>
2019-05-31 15:17:35 -0400

    Added for Login Page Design
    Changed render() to have the html login/register page.
    Changed the imports to import App.css instead of bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import ContactList from "./components/contact_list.component";
 import AddContact from "./components/add_contact.component";
 import UpdateContact from "./components/update_contact.component";
@@ -9,26 +9,40 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#" target="_blank">LOGO</a>
-            <Link to="/" className="navbar-brand">MERN-Stack Contact Manager</Link>
-            <div className="collpase navbar-collapse">
-              <ul className="navbar-nav mr-auto">
-                <li className="navbar-item">
-                  <Link to="/" className="nav-link">Contacts</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/add" className="nav-link">Add Contact</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-          <br/>
-          <Route path="/" exact component={ContactList} />
-          <Route path="/update/:id" component={UpdateContact} />
-          <Route path="/add" component={AddContact} />
-        </div>
+        <input type="checkbox" id="switch"/>
+    
+        <form id="login-form" action="" method="post">
+
+          <header class="header">
+            <h2 class="title">Contact Manager</h2>
+            <p>Login</p> 
+          </header>
+
+          <input type="text" placeholder="Username" required/>
+          <input type="password" placeholder="Password" required/>
+          <footer>
+            <button type="submit">Login</button>
+            <label for="switch"><span>Register</span></label>
+          </footer>
+        </form>
+
+        <form id="register-form" action="" method="post">
+
+          <header class="header">
+            <h2 class="title">Contact Manager</h2>
+            <p>Register</p>
+          </header>
+
+          <input type="fname" placeholder="First Name" required/>
+          <input type="lname" placeholder="Last Name" required/>
+          <input type="email" placeholder="eMail" required/>
+          <input type="text" placeholder="Username" required/>
+          <input type="password" placeholder="Password" required/>
+          <footer>
+            <button type="submit">Register</button>
+            <label for="switch">Already registered? Login</label>
+          </footer>
+        </form>
       </Router>
     );
   }

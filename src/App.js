b4dbf1236b01acb8ed2c11b461476d3696c9500c
sSasquatch <37203953+sSasquatch@users.noreply.github.com>
2019-06-02 10:28:19 -0400

    Updated for Login/Register pages.

--- src/App.js
+++ src/App.js
@@ -1,50 +1,26 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import "./App.css";
+
 import ContactList from "./components/contact_list.component";
 import AddContact from "./components/add_contact.component";
 import UpdateContact from "./components/update_contact.component";
 
+import Register from "./auth/Register";
+import Login from "./auth/Login";
+import Landing from "./components/layout/Landing";
+
 class App extends Component {
   render() {
     return (
-      <Router>
-        <input type="checkbox" id="switch"/>
-    
-        <form id="login-form" action="" method="post">
-
-          <header class="header">
-            <h2 class="title">Contact Manager</h2>
-            <p>Login</p> 
-          </header>
-
-          <input type="text" placeholder="Username" required/>
-          <input type="password" placeholder="Password" required/>
-          <footer>
-            <button type="submit">Login</button>
-            <label for="switch"><span>Register</span></label>
-          </footer>
-        </form>
-
-        <form id="register-form" action="" method="post">
-
-          <header class="header">
-            <h2 class="title">Contact Manager</h2>
-            <p>Register</p>
-          </header>
-
-          <input type="fname" placeholder="First Name" required/>
-          <input type="lname" placeholder="Last Name" required/>
-          <input type="email" placeholder="eMail" required/>
-          <input type="text" placeholder="Username" required/>
-          <input type="password" placeholder="Password" required/>
-          <footer>
-            <button type="submit">Register</button>
-            <label for="switch">Already registered? Login</label>
-          </footer>
-        </form>
-      </Router>
+			<Router>
+				<div className="App">
+				  <Route exact path="/" component={Landing} />
+				  <Route exact path="/login" component={Login} />
+				  <Route exact path="/register" component={Register} />
+				</div>
+			</Router>
     );
   }
 }
 export default App;
+

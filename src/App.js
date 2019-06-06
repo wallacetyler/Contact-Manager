c2a64b9a97080213ff4c50c45d6a2512306412ad
sSasquatch <37203953+sSasquatch@users.noreply.github.com>
2019-06-06 01:26:42 -0400

    fixed the looks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import ContactList from "./components/contact_list.component";
 import AddContact from "./components/add_contact.component";
 import UpdateContact from "./components/update_contact.component";
-
 import Register from "./auth/Register";
 import Login from "./auth/Login";
 import Landing from "./components/layout/Landing";
@@ -17,10 +16,12 @@ class App extends Component {
 				  <Route exact path="/" component={Landing} />
 				  <Route exact path="/login" component={Login} />
 				  <Route exact path="/register" component={Register} />
+				  <Route exact path="/ContactList" component={ContactList} />
+				  <Route exact path="/AddContact" component={AddContact} />
+				  <Route exact path="/UpdateContact" component={UpdateContact} />
 				</div>
 			</Router>
     );
   }
 }
 export default App;
-

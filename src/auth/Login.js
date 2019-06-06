49a62c8993d12fe3a578586bb4fab1c05d993a90
sSasquatch <37203953+sSasquatch@users.noreply.github.com>
2019-06-06 01:21:21 -0400

    fixed the looks

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -1,16 +1,24 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import md5 from "md5"
 
 import "./RegisterLogin.css";
 
+var localApiUrl = "http://localhost:4000";
+var detachedApiUrl = "http://greatcontactmanager.ddns.net:4000";
+
 class Login extends Component {
-  constructor() {
-    super();
+	
+  constructor(props) {
+    super(props);
     this.state = {
       user: "",
       password: "",
-      errors: {}
+      errors: {},
+	  errorMessageVisability: false
     };
+	
+		this.login = this.login.bind(this);
   }
 
   onChange = e => {
@@ -19,28 +27,54 @@ class Login extends Component {
 
   onSubmit = e => {
     e.preventDefault();
-
-	const userData = {
-      user: this.state.user,
-      password: this.state.password
-    };
-
-	console.log(userData);
   };
 
+	login(e) {
+		e.preventDefault();
+		
+		let reqBody = 
+		{
+			user: this.state.user,
+			hash: md5(this.state.password)
+		};
+		
+		var apiURL = window.location.href.includes("localhost")?localApiUrl:detachedApiUrl;
+		fetch(apiURL + '/users/login',
+		{
+			method: 'POST',
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(reqBody)
+		})
+		.then(response => response.json())
+		  .then(data => {
+			  if(data.status === 'failure')
+			  { 
+				this.setState({ errorMessageVisability: true });
+				  
+				console.log('invalid login credentials');
+			  }
+			  else if(data.status === 'success')
+			  {
+				console.log(data.id);
+				console.log('continuing login');
+				window.location.href = "/ContactList?" + data.id;
+			  }
+		  });
+	}
+	
   render() {
     const { errors } = this.state;
 	return (
       <div className="container">
 
-		<header class="header">
-            <h2 class="title">Contact Manager</h2>
-            <p>Login</p> 
+		<header className="header">
+            <h2 className="title">Contact Manager</h2>
+            <p>Login</p>
         </header>
 
 		<form id="login-form" action="" method="post" noValidate onSubmit={this.onSubmit}>
 
-          <input type="text" placeholder="Username" 
+          <input type="user" placeholder="Username" 
 			onChange={this.onChange}
 			value={this.state.user}
 			error={errors.user}
@@ -54,16 +88,14 @@ class Login extends Component {
 			required/>
 
           <footer>
-            <button type="submit">Login</button>
+            <button type="submit" onClick={this.login}>Login</button>
 			<Link to="./Register" id="switch">
-				<label for="switch"><span>Need an account? Register</span></label>
+				<label htmlFor="switch"><span>Need an account? Register</span></label>
 			</Link>
           </footer>
-
-		  <div id="failed">
-			<p>Incorrect login or password! Please try again!</p>
-		  </div>
-
+			<div id="failed">
+				{ this.state.errorMessageVisability ? <p>Invalid username or password.</p> : null } 
+			</div>
         </form>
       </div>
     );

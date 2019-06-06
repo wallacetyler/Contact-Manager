dc8418e3b823bd9bb4ea5891f3b06d61d2fb5368
sSasquatch <37203953+sSasquatch@users.noreply.github.com>
2019-06-06 01:21:50 -0400

    fixed the looks

--- src/auth/Register.js
+++ src/auth/Register.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./RegisterLogin.css";
+import md5 from "md5"
+	
+var localApiUrl = "http://localhost:4000";
+var detachedApiUrl = "http://greatcontactmanager.ddns.net:4000";
 
 class Register extends Component {
-  constructor() {
-    super();
+	
+  constructor(props) {
+    super(props);
     this.state = {
-	user: "",
-      	password: "",
-      	errors: {}
+	  user: "",
+      password: "",
+      errors: {},
+	  errorMessageVisability: false
     };
+	
+	this.doRegister = this.doRegister.bind(this);
   }
 
   onChange = e => {
@@ -21,25 +29,56 @@ class Register extends Component {
 
 	const newUser = {
 	  user: this.state.user,
-      	password: this.state.password,
+      password: this.state.password,
     };
 
 	console.log(newUser);
   };
 
+	doRegister(e) {
+		e.preventDefault();
+		
+		let reqBody = 
+		{
+			user: this.state.user,
+			hash: md5(this.state.password)
+		};
+		
+		var apiURL = window.location.href.includes("localhost")?localApiUrl:detachedApiUrl;
+		fetch(apiURL + '/users/register',
+		{
+			method: 'POST',
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(reqBody)
+		})
+		.then(response => response.json())
+		  .then(data => {
+			  if(data.status === 'failure')
+			  {
+				this.setState({ errorMessageVisability: true });
+				  console.log('username already taken');
+			  }
+			  else
+			  {
+				  console.log('registration successful');
+				  window.location.href = "/Login";
+			  }
+		  });
+	}
+	
   render() {
     const { errors } = this.state;
 	return (
 		<div className="container">
 
-			<header class="header">
-				<h2 class="title">Contact Manager</h2>
+			<header className="header">
+				<h2 className="title">Contact Manager</h2>
 				<p>Register</p>
 			</header>
 
 			<form id="register-form" action="" method="post" noValidate onSubmit={this.onSubmit}>
 
-			  <input type="text" placeholder="Username" 
+			  <input type="user" placeholder="Username" 
 				onChange={this.onChange}
 				value={this.state.user}
 				error={errors.user}
@@ -53,20 +92,17 @@ class Register extends Component {
 				required/>
 
 			  <footer>
-				<button type="submit">Register</button>
+				<button type="submit" onClick={this.doRegister}>Register</button>
 				<Link to="./Login" id="switch">
-					<label for="switch">Already registered? Login</label>
+					<label htmlFor="switch">Already registered? Login</label>
 				</Link>
 			  </footer>
-
 			  <div id="failed">
-				<p>Username is already taken! Please try again!</p>
+				{ this.state.errorMessageVisability ? <p>Username already taken.</p> : null } 
 			  </div>
-
 			</form>
 		</div>
     );
   }
 }
 export default Register;
-
